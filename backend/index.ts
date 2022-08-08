7499102423a2e9df12dec06ed3f48430d01d7a9a
Yeferson Zaque <zaquecamilo@gmail.com>
2022-08-08 13:43:22 -0500

    Preparado el proyecto para que funcione con TypeScript

---
+++ backend/index.ts
@@ -0,0 +1,11 @@
+import express, { Express } from "express";
+
+const app: Express = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
