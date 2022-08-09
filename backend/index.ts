2df22023ec56e42bd09a6979c025085f5f6d34d7
Yeferson Zaque <zaquecamilo@gmail.com>
2022-08-08 21:32:11 -0500

    Aplicada la nueva estructura para la lectura de json y gestión de rutas

--- backend/index.ts
+++ backend/index.ts
@@ -2,16 +2,17 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import { conectDB } from "./config/db";
 
+import usuarioRoutes from "./routes/usuario";
+
 dotenv.config();
 
 const app: Express = express();
+app.use(express.json());
 
 conectDB();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/api/usuarios", usuarioRoutes);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
