28654834e741862903438f8062bd9dbdea020668
Yeferson Zaque <zaquecamilo@gmail.com>
2022-08-09 13:18:05 -0500

    Creado el modelo para los proyectos

---
+++ backend/models/Proyecto.ts
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+export interface IProyectoModel {
+  nombre: string;
+  descripcion: string;
+  fechaEntrega: Date;
+  cliente: string;
+  creador: mongoose.Schema.Types.ObjectId;
+  colaboradores: mongoose.Schema.Types.ObjectId[];
+}
+
+const proyectoSchema = new mongoose.Schema<IProyectoModel>({
+  nombre: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  descripcion: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  fechaEntrega: {
+    type: Date,
+    default: Date.now(),
+  },
+  cliente: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  creador: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Usuario',
+  },
+  colaboradores: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Usuario',
+    }
+  ]
+}, { timestamps: true });
+
+export const Proyecto = mongoose.model<IProyectoModel>('Proyecto', proyectoSchema);
