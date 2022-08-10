62cde6a077b098d0140a6c83a58b5b05351df625
Yeferson Zaque <zaquecamilo@gmail.com>
2022-08-09 22:44:55 -0500

    Implementadas las rutas del área pública

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AuthLayout from './layouts/AuthLayout';
+
+import Login from './pages/Login';
+import Registrar from './pages/Registrar';
+import OlvidePassword from './pages/OlvidePassword';
+import NuevoPassword from './pages/NuevoPassword';
+import ConfirmarCuenta from './pages/ConfirmarCuenta';
+
 function App() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold">Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route element={<Login />} index />
+          <Route path="registrar" element={<Registrar />} />
+          <Route path="olvide-password" element={<OlvidePassword />} />
+          <Route path="olvide-password/:token" element={<NuevoPassword />} />
+          <Route path="confirmar/:token" element={<ConfirmarCuenta />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
