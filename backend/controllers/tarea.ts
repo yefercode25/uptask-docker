edcb60c4debb53b8f225ef75a0c5195df90cbce3
Yeferson Zaque <zaquecamilo@gmail.com>
2022-08-09 21:52:40 -0500

    Agregada la opción de editar y eliminar una tarea especifica

--- backend/controllers/tarea.ts
+++ backend/controllers/tarea.ts
@@ -67,11 +67,78 @@ export const agregarTarea = async (req: Request, res: Response) => {
 }
 
 export const editarTarea = async (req: Request, res: Response) => {
+  const { nombre, descripcion, prioridad, fechaEntrega } = req.body;
+  const { id } = req.params;
 
+  try {
+    const tarea = await Tarea.findById(id).populate('proyecto');
+    if(!tarea) {
+      const error = new Error('La tarea no existe');
+      return res.status(404).json({
+        ok: false,
+        msg: error.message
+      });
+    }
+
+    if((tarea.proyecto as IProyectoModel).creador.toString() !== req.usuario._id.toString()){
+      const error = new Error('No tienes permisos para editar esta tarea');
+      return res.status(401).json({
+        ok: false,
+        msg: error.message
+      });
+    }
+
+    tarea.nombre = nombre || tarea.nombre;
+    tarea.descripcion = descripcion || tarea.descripcion;
+    tarea.prioridad = prioridad || tarea.prioridad;
+    tarea.fechaEntrega = fechaEntrega || tarea.fechaEntrega;
+
+    await tarea.save();
+
+    res.json(tarea);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Error al editar la tarea'
+    });
+  }
 }
 
 export const eliminarTarea = async (req: Request, res: Response) => {
+  const { id } = req.params;
 
+  try {
+    const tarea = await Tarea.findById(id).populate('proyecto');
+    if(!tarea) {
+      const error = new Error('La tarea a eliminar no existe');
+      return res.status(404).json({
+        ok: false,
+        msg: error.message
+      });
+    }
+
+    if((tarea.proyecto as IProyectoModel).creador.toString() !== req.usuario._id.toString()){
+      const error = new Error('No tienes permisos para eliminar esta tarea');
+      return res.status(401).json({
+        ok: false,
+        msg: error.message
+      });
+    }
+
+    await tarea.deleteOne();
+
+    res.json({
+      ok: true,
+      msg: 'Tarea eliminada'
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Error al eliminar la tarea'
+    });
+  }
 }
 
 export const cambiarEstado = async (req: Request, res: Response) => {
