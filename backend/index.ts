62e91ba03e3d26d67f8e48fd7b108b5cac445b80
Yeferson Zaque <zaquecamilo@gmail.com>
2022-08-10 13:48:45 -0500

    Implementado el manejo de peticiones por otro servidor con CORS

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { conectDB } from './config/db';
 
 import usuarioRoutes from './routes/usuario';
@@ -10,6 +11,18 @@ dotenv.config();
 
 const app: Express = express();
 app.use(express.json());
+
+// Configurar CORS
+const whitelist = ['http://localhost:5173'];
+app.use(cors({
+  origin: (origin, callback) => { 
+    if(!whitelist.includes(origin!)) {
+      return callback(new Error('No tienes permiso para hacer peticiones a este servidor'));
+    }
+    
+    callback(null, true);
+  }
+}));
 
 conectDB();
 
